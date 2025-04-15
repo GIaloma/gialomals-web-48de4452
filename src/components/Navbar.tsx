5f29f6c9aa7e836675ca450fd713e11bae1d3061
Gialoma <gialomals@gmail.com>
2025-04-15 20:07:16 +0200

    Reduce navbar size on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,14 +21,14 @@ const Navbar = () => {
   }, []);
   
   return (
-    <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? 'bg-white shadow-md py-3' : 'bg-transparent py-5'}`}>
+    <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? 'bg-white shadow-md py-2 md:py-3' : 'bg-transparent py-3 md:py-5'}`}>
       <div className="container mx-auto px-4 flex justify-between items-center">
         <div className="flex items-center">
           <a href="/" className="flex items-center">
             <img 
               alt="Gialoma Life Solutions Logo" 
               src="/lovable-uploads/d3975bb5-3e96-450e-a77f-7fd8af9e04de.png" 
-              className="h-24 w-auto mr-3 object-contain" 
+              className="h-16 md:h-24 w-auto mr-3 object-contain transition-all duration-300" 
             />
           </a>
         </div>
@@ -56,7 +56,7 @@ const Navbar = () => {
 
           {/* Mobile Menu Button */}
           <button className="md:hidden text-gialoma-black" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+            {isMenuOpen ? <X size={22} /> : <Menu size={22} />}
           </button>
         </div>
       </div>
