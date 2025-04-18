fddd89a59a5138be8f93655e70d3ae8692dc2a35
Gialoma <gialomals@gmail.com>
2025-04-18 19:15:08 +0200

    Add Cookie Policy route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import BlogPost from "./pages/BlogPost";
 import BlogIndex from "./pages/BlogIndex";
 import Login from "./pages/Login";
 import ClientDashboard from "./pages/ClientDashboard";
+import CookiePolicy from "./pages/CookiePolicy";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -25,6 +26,7 @@ const App = () => (
           <Route path="/blog/:slug" element={<BlogPost />} />
           <Route path="/login" element={<Login />} />
           <Route path="/client-dashboard" element={<ClientDashboard />} />
+          <Route path="/cookie-policy" element={<CookiePolicy />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
