10ec58feac341fcbe3ae6df4be9c2b92d537826f
Gialoma <gialomals@gmail.com>
2025-04-23 18:18:10 +0200

    Add simplified LoginNavbar component with only logo linked to homepage

---
+++ src/components/LoginNavbar.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LoginNavbar = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-white shadow-md py-2 md:py-3">
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        <div className="flex items-center">
+          <Link to="/" className="flex items-center">
+            <img 
+              alt="Gialoma Life Solutions Logo" 
+              src="/lovable-uploads/d3975bb5-3e96-450e-a77f-7fd8af9e04de.png" 
+              className="h-16 md:h-24 w-auto mr-3 object-contain transition-all duration-300" 
+            />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default LoginNavbar;
