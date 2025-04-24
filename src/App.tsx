c696eb2424fcf66591d5548a63955115fc5dbe21
Gialoma <gialomals@gmail.com>
2025-04-24 17:32:05 +0200

    Update App.tsx to add digitalization page route

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import CookiePolicy from "./pages/CookiePolicy";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 import Book from "./pages/Book";
+import Digitalization from "./pages/Digitalization";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -35,6 +36,7 @@ const App = () => (
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="/terms-of-service" element={<TermsOfService />} />
           <Route path="/book" element={<Book />} />
+          <Route path="/digitalization" element={<Digitalization />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
