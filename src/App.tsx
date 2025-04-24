86040bb5cbfabf0f578c1617c681ccce826b721d
Gialoma <gialomals@gmail.com>
2025-04-24 16:34:40 +0200

    Update App.tsx to include route for Terms of Service page

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import ClientDashboard from "./pages/ClientDashboard";
 import FounderDashboard from "./pages/FounderDashboard";
 import CookiePolicy from "./pages/CookiePolicy";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
 import Book from "./pages/Book";
 import NotFound from "./pages/NotFound";
 
@@ -32,6 +33,7 @@ const App = () => (
           <Route path="/founder-dashboard" element={<FounderDashboard />} />
           <Route path="/cookie-policy" element={<CookiePolicy />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
           <Route path="/book" element={<Book />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
