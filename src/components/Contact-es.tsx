03ff93f992b43f96278209c6326d9220ae54e212
Gialoma <gialomals@gmail.com>
2025-05-31 17:18:26 +0200

    Update Contact-es component to change email to gialoma@gialoma.com

--- src/components/Contact-es.tsx
+++ src/components/Contact-es.tsx
@@ -96,7 +96,7 @@ const ContactEs = () => {
                 </div>
                 <div>
                   <h4 className="font-semibold text-gialoma-black mb-1">Escríbenos</h4>
-                  <p className="text-gialoma-darkgray">gialomals@gmail.com</p>
+                  <p className="text-gialoma-darkgray">gialoma@gialoma.com</p>
                 </div>
               </div>
             </div>
