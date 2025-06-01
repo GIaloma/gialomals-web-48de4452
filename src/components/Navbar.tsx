deeb114791b944a0db51a6e856c9766a7c06d1b6
Gialoma <gialomals@gmail.com>
2025-06-01 18:49:14 +0200

    Update navbar logos to link to gialoma.com for both Spanish and English versions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,13 +25,13 @@ const Navbar = () => {
     <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? 'bg-white shadow-md py-2 md:py-3' : 'bg-transparent py-3 md:py-5'}`}>
       <div className="container mx-auto px-4 flex justify-between items-center">
         <div className="flex items-center">
-          <Link to="/en" className="flex items-center">
+          <a href="https://gialoma.com" className="flex items-center">
             <img 
               alt="Gialoma Life Solutions Logo" 
               src="/lovable-uploads/d3975bb5-3e96-450e-a77f-7fd8af9e04de.png" 
               className="h-14 md:h-20 w-auto mr-3 object-contain transition-all duration-300" 
             />
-          </Link>
+          </a>
         </div>
 
         {/* Desktop Menu */}
